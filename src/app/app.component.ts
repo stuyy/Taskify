c6d772d4d9b04b53a967f2e51b5ec2dad84e5b62
ansonfoong <afoong@albany.edu>
2019-06-28 16:08:43 -0400

    Current setup of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('newTask', [
+      state('hidden', style({
+        opacity: 0
+      })),
+      state('show', style({
+        opacity: 1
+      })),
+      transition('hidden <=> show', [
+        animate('0.2s ease-in')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'TaskAppNg';
+  buttonTaskTitle='New Task';
+  taskTitle = '';
+  taskDescription = '';
+  show = false;
+  newTask()
+  {
+    this.show = !this.show;
+    if(this.show)
+      this.buttonTaskTitle='Cancel';
+    else
+    {
+      this.buttonTaskTitle='New Task'
+      this.taskTitle = '';
+      this.taskDescription = '';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import {  FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,FormsModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
